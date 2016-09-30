48eef9e3cfa11ff30cf1cac5124ffe59000c69c3
Megan O'Neill <mo2638@columbia.edu>
2016-09-29 23:55:53 -0400

    add a few items to footer

--- footer.php
+++ footer.php
@@ -2,14 +2,43 @@
 		
 		<footer class="footer">
 			
-			<div class="container">
-		        <div class="col-sm-4">
+			<div class="col-sm-12">
+
+		        <div class="col-sm-4 logo-footer">
 					<img class="lions-logo" src="<?php echo get_template_directory_uri(); ?>/lions.png"/>
 		        </div>
 
-		        <div class="col-sm-8">
+		        <div class="col-sm-4 social-footer">
+					
+					<a href="https://twitter.com/villagelions">
+				        <span class="fa-stack fa-lg">
+				          <i class="fa fa-circle fa-stack-2x"></i>
+				          <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+				        </span>
+			    	</a>
+			        
+			        <a href="https://www.facebook.com/villagelions">
+			          <span class="fa-stack fa-lg">
+			            <i class="fa fa-circle fa-stack-2x"></i>
+			            <i class="fa fa-facebook fa-stack-1x fa-inverse"></i>
+			          </span>
+			        </a>
 
+			        <a href="mailto:wcb@villagelions.org">
+			          <span class="fa-stack fa-lg">
+			            <i class="fa fa-circle fa-stack-2x"></i>
+			            <i class="fa fa-envelope-o fa-stack-1x fa-inverse"></i>
+			          </span>
+			        </a>
 		        </div>
+
+		        <div class="col-sm-4 contact-footer">
+		        	<ul>
+		        		<li>Questions? Contact us.</li>
+		        		<li><a href="mailto:wcb@villagelions.org">wcb@villagelions.org</a></li>
+		        	</ul>
+		        </div>
+
 		    </div>
 
 		</footer>
