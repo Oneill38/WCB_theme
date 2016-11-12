2dffb982f3d2f4151221ef5cd06f5ad1bd8d9d13
Megan O'Neill <mo2638@columbia.edu>
2016-11-12 14:07:46 -0500

    Closes 1
    Fixes link color so that the white ones will remain, and the rest have the standard bootstrap hex color.

--- page.php
+++ page.php
@@ -32,7 +32,7 @@
       <hr>
 
        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-             <div>
+             <div class="page-text">
                <?php the_content(); ?>
              </div>
       <?php endwhile; endif; ?>
